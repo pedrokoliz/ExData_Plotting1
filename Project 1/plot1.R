932b9c2f42c6a1fc903e990118501ff3eb29d2be
pedrokoliz <pamc.1599@gmail.com>
2015-05-09 11:46:18 -0300

    Project1

---
+++ Project 1/plot1.R
@@ -0,0 +1,6 @@
+file <- file("household_power_consumption.txt")
+data <-  read.table(text = grep("^[1,2]/2/2007", readLines(file), value = TRUE), header = TRUE, sep = ";",  col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+png(file = "myplot.1")
+hist(x = data$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "red", ylab = "Frequency", main = paste("Global Active Power"))
+dev.off()
---
+++ Project 1/plot2.R
@@ -0,0 +1,6 @@
+file <- file("household_power_consumption.txt")
+data <-  read.table(text = grep("^[1,2]/2/2007", readLines(file), value = TRUE), header = TRUE, sep = ";",  col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+png(file = "plot2.png")
+plot(x = datetime, y = data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
